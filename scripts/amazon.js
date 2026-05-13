ef56815833067fdd643867e9b68fc6923a4b0e05
SuperSimpleDev <noks@supersimple.dev>
2026-05-13 23:45:31 +1000

    Use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,53 +1,27 @@
-const products = [{
-    Image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    Image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    Image : "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
-
 let productsHTML = '';
 
-products.forEach((products) => {
+products.forEach((product) => {
     productsHTML +=`
     <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${products.Image}">
+              src="${product.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
-            ${products.name}
+            ${product.name}
           </div>
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${products.rating.stars * 10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              ${products.rating.count}
+              ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(products.priceCents / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
